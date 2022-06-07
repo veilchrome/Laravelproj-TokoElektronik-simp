13a219f1479a55625749b0639fba0c015e8681bd
Selamet Saputra <keeita768900@gmail.com>
2022-06-07 08:52:40 +0700

    Commit Pertama

---
+++ resources/views/templates/bottom_menu.blade.php
@@ -0,0 +1,19 @@
+<nav class="bottom-navbar">
+    <div class="container">
+        <ul class="nav page-navigation">
+            <li class="nav-item">
+                <a class="nav-link" href="/">
+                    <i class="link-icon" data-feather="box"></i>
+                    <span class="menu-title">Dashboard</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="{{url('products')}}">
+                    <i class="link-icon" data-feather="box"></i>
+                    <span class="menu-title">Products</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
